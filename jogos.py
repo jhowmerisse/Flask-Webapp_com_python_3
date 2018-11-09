2e469ce0bf15cf0e2e4fc9f22e5a387e507ce9f8
jhowmerisse <jhow.merisse@hotmail.com>
2018-11-09 14:07:44 -0200

    implementando protecao de rota e url dinamica

--- jogos.py
+++ jogos.py
@@ -1,7 +1,7 @@
-from flask import Flask, render_template, request, redirect, session, flash
+from flask import Flask, render_template, request, redirect, session, flash, url_for
 
 app = Flask(__name__)
-app.secret_key = 'jhow'
+app.secret_key = 'alura'
 
 class Jogo:
     def __init__(self, nome, categoria, console):
@@ -10,46 +10,49 @@ class Jogo:
         self.console = console
 
 jogo1 = Jogo('Super Mario', 'Ação', 'SNES')
-jogo2 = Jogo('Pokemon', 'RGB', 'GBA')
-jogo3 = Jogo('Mortal Kombat', 'Luta', 'SNES')
-lista = [jogo1, jogo2, jogo3]
+jogo2 = Jogo('Pokemon Gold', 'RPG', 'GBA')
+lista = [jogo1, jogo2]
 
 @app.route('/')
 def index():
-    return render_template('lista.html', titulo='Jogos',
-                            jogos=lista)
+    return render_template('lista.html', titulo='Jogos', jogos=lista)
 
 @app.route('/novo')
 def novo():
+    if 'usuario_logado' not in session or session['usuario_logado'] == None:
+        return redirect(url_for('login', proxima=url_for('novo')))
     return render_template('novo.html', titulo='Novo Jogo')
 
 @app.route('/criar', methods=['POST',])
 def criar():
-    nome = request.form['nome']
-    categoria= request.form['categoria']
-    console = request.form['console']
+    nome = request. form['nome']
+    categoria = request. form['categoria']
+    console = request. form['console']
     jogo = Jogo(nome, categoria, console)
     lista.append(jogo)
-    return redirect('/')
+    return redirect(url_for('index'))
 
 @app.route('/login')
 def login():
-    return render_template('login.html')
+    proxima = request.args.get('proxima')
+    return render_template('login.html', proxima=proxima)
 
-@app.route('/autenticar', methods=['POST',])
+
+@app.route('/autenticar', methods=['POST', ])
 def autenticar():
     if 'teste' == request.form['senha']:
         session['usuario_logado'] = request.form['usuario']
         flash(request.form['usuario'] + ' logou com sucesso!')
-        return redirect('/')
+        proxima_pagina = request.form['proxima']
+        return redirect(proxima_pagina)
     else:
-        flash('Não logado, tente novamente')
-        return redirect('/login')
+        flash('Não logado, tente novamente!')
+        return redirect(url_for('login'))
 
 @app.route('/logout')
 def logout():
     session['usuario_logado'] = None
     flash('Nenhum usuário logado!')
-    return redirect('/')
+    return redirect(url_for('index'))
 
 app.run(debug=True)
